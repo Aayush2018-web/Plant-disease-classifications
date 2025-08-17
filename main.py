3f15cd68b07267ee768541669a1b3c71262b7347
Aayush2018-web <aayushpatil2018@gmail.com>
2025-08-17 16:07:29 +0530

    main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI, File, UploadFile
-from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 import numpy as np
 from io import BytesIO
@@ -40,6 +39,7 @@ async def predict(
     }
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=False)
+
 
 
 
