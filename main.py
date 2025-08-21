74aaab8e588a5d756e11839c878623e381b91416
Aayush2018-web <aayushpatil2018@gmail.com>
2025-08-21 22:02:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,18 @@ import tensorflow as tf
 
 app = FastAPI()
 
+origins = [
+    "https://pdc-frontend-jeg6.onrender.com",
+    "http://localhost:3000",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 MODEL = tf.keras.models.load_model("saved_models/1.keras")
 
 CLASS_NAMES = ["Early Blight", "Late Blight", "Healthy"]
@@ -33,3 +45,4 @@ async def predict(
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=False)
     
+
